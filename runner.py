031d14e6d2cae26d0ecdc8bb0d3bd056634b1e99
Martin Sundhaug <martinsundhaug@gmail.com>
2025-07-11 19:37:48 +0200

    Good day

--- runner.py
+++ runner.py
@@ -1,30 +1,32 @@
 import json, os, sys, time, subprocess
 
 def main():
-    while True:
-        j = None
-        try:
-            with open('jobs.json') as f:
-                j = json.load(f)
+    with open('runner.log', 'w') as log_file:
+        while True:
+            j = None
+            try:
+                file_change_time = os.path.getmtime('jobs.json')
+                with open('jobs.json') as f:
+                    j = json.load(f)
 
-                os.chdir('C:\\Users\\Sundh\\Desktop\\pentest\\hashcat-6.2.5')
-                jobs_done = []
-                for job in j:
-                    if job in jobs_done:
-                        continue
-                    (digest, left, right) = job
-                    cmd = r'hashcat -m 1400 -a 1 --potfile-path C:\Users\Sundh\Desktop\dev\PeterSchmidtNielsen\secrets2.pot '
-                    cmd += f'{digest} {left} {right}'
-                    subprocess.run(cmd, shell=True)
-                    jobs_done.append(job)
+                    os.chdir('C:\\Users\\Sundh\\Desktop\\pentest\\hashcat-6.2.5')
+                    for job in j:
+                        if file_change_time != os.path.getmtime(r'C:\Users\Sundh\Desktop\dev\PeterSchmidtNielsen\jobs.json'):
+                            break
+                        (digest, left, right) = job
+                        cmd = r'hashcat -m 1400 -a 1 --potfile-path C:\Users\Sundh\Desktop\dev\PeterSchmidtNielsen\secrets2.pot '
+                        cmd += f'{digest} {left} {right}'
+                        subprocess.run(cmd, shell=True)
+                        print(f'Executed: {cmd}')
+                        print(cmd, file=log_file, flush=True)
 
-                os.chdir('C:\\Users\\Sundh\\Desktop\\dev\\PeterSchmidtNielsen')
-        except FileNotFoundError as e:
-            print(e)
-            time.sleep(5)
-        except json.JSONDecodeError as e:
-            print(e)
-            time.sleep(5)
+                    os.chdir('C:\\Users\\Sundh\\Desktop\\dev\\PeterSchmidtNielsen')
+            except FileNotFoundError as e:
+                print(e)
+                time.sleep(5)
+            except json.JSONDecodeError as e:
+                print(e)
+                time.sleep(5)
 
 
 if __name__ == '__main__':

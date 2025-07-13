6f948f2cdaa81f81f488894b64488a262c72d3cd
Martin Sundhaug <martinsundhaug@gmail.com>
2025-07-13 19:10:20 +0200

    Good day, fixed some code

--- estimate-runtime.py
+++ estimate-runtime.py
@@ -2,8 +2,9 @@ from datetime import datetime, timedelta
 import random
 
 start_time = datetime.now()
-start_bit_cnt = 60
+start_bit_cnt = 61
 hash_rate = 4_400_000_000
+assume_bits = 0
 
 def do_experiment():
     end_date = start_time
@@ -12,9 +13,13 @@ def do_experiment():
 
     while bit_solved < 65:
         bits_known = (end_date - datetime(2025, 7, 1)).days + 1
-        difficulty = random.randint(1, 2 ** (bit_solved - bits_known))
+        difficulty = 2 ** (bit_solved - bits_known - assume_bits)
+        if random.random() < 2 ** (-assume_bits):
+            difficulty = random.randint(1, difficulty)
+
         # print(f"Difficulty: {difficulty} for {bit_solved} bits")
         delta_time = timedelta(seconds=difficulty / hash_rate)
+        # print(f"Delta time: {delta_time} for {bit_solved} bits @ day {(end_date - start_time).days + 1}")
         if delta_time.days > 1:
             continue
         # print(f"Delta time: {delta_time} for {bit_solved} bits")
@@ -25,5 +30,5 @@ def do_experiment():
     print((end_date - start_time).total_seconds())
 
 if __name__ == "__main__":
-    for i in range(10_000):
+    for i in range(100_000):
         do_experiment()

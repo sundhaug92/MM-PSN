3664d60aae6e35958e187045cf21a34f4c31a660
Martin Sundhaug <martinsundhaug@gmail.com>
2025-07-14 03:07:36 +0200

    Configurability

--- regen2-prep.py
+++ regen2-prep.py
@@ -1,6 +1,7 @@
 import json
 
-min_left_side_bits = 35
+min_left_side_bits = 37
+extra_bits = 0
 
 with open('jobs.json') as f:
     j = json.load(f)
@@ -14,4 +15,4 @@ with open('jobs.json') as f:
         if left_side_bits < min_left_side_bits:
             continue
 
-        print(f'python3 regen2.py $(({left_side_bits}+25)) {left_side_prefix}' + '{0..1}'*(left_side_bits - min_left_side_bits + 2))
+        print(f'python3 regen2.py $(({left_side_bits}+25)) {left_side_prefix}' + '{0..1}'*(left_side_bits - min_left_side_bits + 1))

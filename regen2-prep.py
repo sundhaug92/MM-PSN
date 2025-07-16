06a9b86e1671d2e98c96d01046c2666b400ed892
Martin Sundhaug <martinsundhaug@gmail.com>
2025-07-16 10:58:23 +0200

    New feature auto-set min

--- regen2-prep.py
+++ regen2-prep.py
@@ -1,6 +1,6 @@
 import json
 
-min_left_side_bits = 37
+min_left_side_bits = None
 extra_bits = 0
 
 with open('jobs.json') as f:
@@ -12,7 +12,11 @@ with open('jobs.json') as f:
         left_side_bits = int(left_side_bits_str)
         left_side_prefix = left.split('.')[2]
 
+        if min_left_side_bits is None:
+            min_left_side_bits = left_side_bits
+
         if left_side_bits < min_left_side_bits:
             continue
 
-        print(f'python3 regen2.py $(({left_side_bits}+25)) {left_side_prefix}' + '{0..1}'*(left_side_bits - min_left_side_bits + 1))
+        for i in range(left_side_bits - min_left_side_bits + 1):
+            print(f'python3 regen2.py $(({left_side_bits}+25)) {left_side_prefix}' + '{0..1}' * i)
